43c50aee5e279b91a565e905849288a9354edda9
mahek-mistry <mahek.mistry17@gmail.com>
2023-04-01 16:18:50 +0530

    B commit

---
+++ characterFrequeny.py
@@ -0,0 +1,8 @@
+n=input("enter the string:")
+s={}    #dictionary called
+for i in n:
+    if i in s:
+        s[i]+=1   #letters counted
+    else:
+        s[i]=1
+print(s)
---
+++ subStringCount.py
@@ -0,0 +1,3 @@
+str=input("enter a string: ")
+sub=input("enter sub string to check:")
+print(str.count(sub))
